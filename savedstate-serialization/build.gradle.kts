ad2a902e8e1ee47ccf9485509737e361112030fc
FCD 黄子耀 <ziyao.huang@fenrir-inc.com.cn>
2025-08-08 18:42:31 +0800

    publish to maven(JitPack)

--- savedstate-serialization/build.gradle.kts
+++ savedstate-serialization/build.gradle.kts
@@ -3,7 +3,10 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
+
+group = "com.github.wshzy"
 
 android {
     namespace = "ziyao.huang.savedstate.gson"
